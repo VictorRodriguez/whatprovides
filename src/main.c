7c0cc843993cd7420045b5c81caf97f6c1bf3b97
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2018-10-20 04:29:59 +0000

    Enable whatprovides by dnf-3
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main (){
+
+void print_help(char** argv){
+
+    printf("\nUse this binary to search what pkg provides a binary\n");
+    printf("\ni.e.:\n");
+    printf("\n    %s /usr/bin/ls\n\n",argv[0]);
+
+}
+
+int main (int argc, char** argv){
+
+    const char *fname = "/etc/dnf.conf";
+    if( access( fname, F_OK ) != -1 ) {
+        if (argc>=2){
+        char *const parmlist[] = {"/usr/bin/dnf-3", "--releasever=clear",\
+            "--config=/etc/dnf.conf","provides",argv[1],NULL};
+        execv("/usr/bin/dnf-3",parmlist);
+        } else{
+            print_help(argv);
+            return -1;
+        }
+    } else {
+        perror("dnf.does not exist in /etc/, please copy there");
+        return -1;
+    }
+
+
     return 0;
+
 }
 
